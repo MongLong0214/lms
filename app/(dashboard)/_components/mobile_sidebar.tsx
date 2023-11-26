341d3563883c6ce31b46fcc39522ae5a8fa24531
MongLong0214 <weplay0628@gmail.com>
2023-11-26 16:26:19 +0900

    mobile sidebar add

--- app/(dashboard)/_components/mobile_sidebar.tsx
+++ app/(dashboard)/_components/mobile_sidebar.tsx
@@ -2,11 +2,21 @@
 
 import React from "react";
 import { Menu } from "lucide-react";
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import Sidebar from "@/app/(dashboard)/_components/sidebar";
 
 interface MobileSidebarProps {}
 
 function MobileSidebar({}: MobileSidebarProps) {
-  return <Menu />;
-}
+  return (
+  <Sheet>
+    <SheetTrigger className='md:hidden pr-4 hover:opacity-75 transition'>
+      <Menu />
+    </SheetTrigger>
+    <SheetContent side='left' className='p-0 bg-white'>
+        <Sidebar />
+    </SheetContent>
+  </Sheet>
+  )}
 
 export default MobileSidebar;
