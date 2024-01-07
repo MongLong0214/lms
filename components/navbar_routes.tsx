b74c5030f4287ceb4387cd192063ee3ecef85366
MongLong0214 <weplay0628@gmail.com>
2024-01-07 16:00:26 +0900

    prisma 설치

--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -1,49 +1,39 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { UserButton } from '@clerk/nextjs'
-import {usePathname} from "next/navigation";
-import {Button} from "@/components/ui/button";
-import {LogOut} from "lucide-react";
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 import Link from "next/link";
 
-interface NavbarRoutesProps {
-
-}
+interface NavbarRoutesProps {}
 
 function NavbarRoutes({}: NavbarRoutesProps) {
-    const pathname = usePathname()
+  const pathname = usePathname();
 
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
 
-    const isTeacherPage = pathname?.startsWith("/teacher")
-    const isPlayerPage = pathname?.includes("/chapter")
-
-    return (
-        <div className='flex gap-x-2 ml-auto'>
-            {isTeacherPage || isPlayerPage ? (
-                <Link href='/'>
-                <Button size='sm' variant='ghost'>
-                    <LogOut
-                     className='h-4 w-4 mr-2'
-                    />
-                    Exit
-                </Button>
-                </Link>
-
-            ) :
-                (
-                    <Link href='/teacher/courses'>
-                        <Button size='sm' variant='ghost'>
-                        Teacher Mode
-                        </Button>
-                    </Link>
-                )
-            }
-            <UserButton
-            afterSignOutUrl=""
-            />
-        </div>
-    )
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="" />
+    </div>
+  );
 }
 
 export default NavbarRoutes;
